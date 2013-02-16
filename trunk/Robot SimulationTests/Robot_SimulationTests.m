a93462986e3fdf276e6debd6f66bad0895a1fc9b
bmdelaune@gmail.com <bmdelaune@gmail.com@00347c2a-44e5-a4e8-ba2b-af20475418ce>
2013-02-16 21:20:19 +0000

    Initial Upload

---
+++ trunk/Robot SimulationTests/Robot_SimulationTests.m
@@ -0,0 +1,32 @@
+//
+//  Robot_SimulationTests.m
+//  Robot SimulationTests
+//
+//  Created by Bradley Delaune on 2/14/13.
+//  Copyright (c) 2013 Senior Design. All rights reserved.
+//
+
+#import "Robot_SimulationTests.h"
+
+@implementation Robot_SimulationTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Robot SimulationTests");
+}
+
+@end
